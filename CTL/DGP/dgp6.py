4d134126b052ca1262618fff8a8cbe94633e904a
nominmar <margad-erdene_nomin@student.ceu.edu>
2021-04-26 09:57:32 +0200

    ate4 mc sims

--- CTL/DGP/dgp6.py
+++ CTL/DGP/dgp6.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def dgp6(n, var_e):
+def dgp_ate4(n, var_e):
     np.random.seed()
     #treatment (randomly assigned)
     d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
@@ -41,3 +41,34 @@ def dgp6(n, var_e):
 #print(np.mean(y[np.where((x1 >= 0) & (x2 < 0) & (d == 1))]) - np.mean(y[np.where((x1 >= 0) & (x2 < 0) & (d == 0))]))
 #print(np.mean(y[np.where((x1 < 0) & (x2 >= 0) & (d == 1))]) - np.mean(y[np.where((x1 < 0) & (x2 >= 0) & (d == 0))]))
 #print(np.mean(y[np.where((x1 < 0) & (x2 < 0) & (d == 1))]) - np.mean(y[np.where((x1 < 0) & (x2 < 0) & (d == 0))]))
+
+def dgp_ate4_int(n, var_e):
+    np.random.seed()
+    #treatment (randomly assigned)
+    d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
+    #covariates (relevate to T.E.)
+    x1 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x2 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    #covariates (relevant to outcome)
+    x3 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x4 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x5 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    #covariates (irrelevant)
+    x6 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    #error term
+    e = np.random.normal(loc = 0.0, scale = var_e, size = (n,1))
+
+    #indicator functions
+    ind1 = np.zeros((n,1))
+    ind2 = np.zeros((n,1))
+    ind3 = np.zeros((n,1))
+    ind1[np.where(x1 >= 0)] = 1
+    ind2[np.where(x2 >= 0)] = 1
+    ind3[np.where((x1 >= 0) & (x2 >= 0))] = 1
+
+    y = np.add(np.add(np.add(np.add(np.add(d*-2, np.multiply(np.add(np.add(3*ind1, ind2), ind3),d)),x3),x4),x5),e)
+    X = np.concatenate((x1,x2,x3,x4,x5,x6), axis=1)
+    
+    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=0.5)
+    
+    return x_train, x_test, y_train, y_test, treat_train, treat_test
