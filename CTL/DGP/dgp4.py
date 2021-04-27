1c6cb071664078791cbf26c53f6deec9a7904ed5
nominmar <margad-erdene_nomin@student.ceu.edu>
2021-04-27 03:03:08 +0200

    updates

--- CTL/DGP/dgp4.py
+++ CTL/DGP/dgp4.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def dgp4(n):
+def dgp4(n, var_e):
     np.random.seed()
     p=[1./2, 1./2]
     d = np.random.choice([0, 1], size=(n,1), p=p)
@@ -10,24 +10,14 @@ def dgp4(n):
     x2 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
     x3 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
     x4 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))    
-    e = np.random.normal(loc = 0.0, scale = 0.01, size = (n,1))
-    #print(x1)
+    e = np.random.normal(loc = 0.0, scale = var_e, size = (n,1))
+
     ind = np.zeros((n,1))
-    #print(d)
     ind[np.where(x1 >= 0)] = 1
-    #print(ind)
-    y1 = np.add(d*-1, np.multiply(2, np.multiply(ind,d)))
-    #print(y1)
-    y2 = np.add(y1, x2)
-    y3 = np.add(y2, x3)
-    y4 = np.add(y3, x4)
-    y = np.add(y4, e)
+    y = np.add(np.add(np.add(np.add(np.add(d*-1.5, np.multiply(3*d, ind)),x2),x3),x4),e)
 
+    X = np.concatenate((x1,x2,x3,x4), axis=1)
 
-
-    X = np.concatenate((x1,x2), axis=1)
-    X = np.concatenate((X,x3), axis=1)
-    X = np.concatenate((X,x4), axis = 1) #covariates
 
     #X = np.concatenate((X,d), axis = 1)
     x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=0.5)
