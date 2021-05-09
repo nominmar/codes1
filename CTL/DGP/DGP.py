421276d976ee176accfcd9ef57387b55c4ae188b
nominmar <margad-erdene_nomin@student.ceu.edu>
2021-05-09 15:06:34 +0200

    updated running files

--- CTL/DGP/DGP.py
+++ CTL/DGP/DGP.py
@@ -2,7 +2,29 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def dgp_ate4(n, var_e):
+
+def dgp2(n, tsize, var_e):
+    np.random.seed()
+    p=[1./2, 1./2]
+    d = np.random.choice([0, 1], size=(n,1), p=p)
+    x1 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x2 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x3 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
+    x4 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))    
+    e = np.random.normal(loc = 0.0, scale = var_e, size = (n,1))
+
+    ind = np.zeros((n,1))
+    ind[np.where(x1 >= 0)] = 1
+    y = np.add(np.add(np.add(np.add(np.add(d*-1.5, np.multiply(3*d, ind)),x2),x3),x4),e)
+    X = np.concatenate((x1,x2,x3,x4), axis=1)
+
+    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=tsize)
+
+    return x_train, x_test, y_train, y_test, treat_train, treat_test
+
+
+
+def dgp4(n, tsize, var_e):
     np.random.seed()
     #treatment (randomly assigned)
     d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
@@ -27,7 +49,7 @@ def dgp_ate4(n, var_e):
     y = np.add(np.add(np.add(np.add(np.add(d*-2, np.multiply(np.add(3*ind1, ind2),d)),x3),x4),x5),e)
     X = np.concatenate((x1,x2,x3,x4,x5,x6), axis=1)
     
-    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=0.5)
+    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=tsize)
     
     return x_train, x_test, y_train, y_test, treat_train, treat_test
 
@@ -42,7 +64,7 @@ def dgp_ate4(n, var_e):
 #print(np.mean(y[np.where((x1 < 0) & (x2 >= 0) & (d == 1))]) - np.mean(y[np.where((x1 < 0) & (x2 >= 0) & (d == 0))]))
 #print(np.mean(y[np.where((x1 < 0) & (x2 < 0) & (d == 1))]) - np.mean(y[np.where((x1 < 0) & (x2 < 0) & (d == 0))]))
 
-def dgp_ate4_int(n, var_e):
+def dgp4int(n, tsize, var_e):
     np.random.seed()
     #treatment (randomly assigned)
     d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
@@ -69,12 +91,12 @@ def dgp_ate4_int(n, var_e):
     y = np.add(np.add(np.add(np.add(np.add(d*-3, np.multiply(np.add(np.add(4*ind1, ind2), ind3),d)),x3),x4),x5),e)
     X = np.concatenate((x1,x2,x3,x4,x5,x6), axis=1)
     
-    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=0.5)
+    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=tsize)
     
     return x_train, x_test, y_train, y_test, treat_train, treat_test
 
 
-def dgp_ate8(n, var_e):
+def dgp8(n, tsize, var_e):
     np.random.seed()
     #treatment (randomly assigned)
     d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
@@ -101,35 +123,7 @@ def dgp_ate8(n, var_e):
 
     y = np.add(np.add(np.add(np.add(d*-5, np.multiply(np.add(np.add(6*ind1, 2.5*ind2),1.5*ind3),d)),x4),x5),e)
     X = np.concatenate((x1,x2,x3,x4,x5,x6), axis=1)
+
+    x_train, x_test, y_train, y_test, treat_train, treat_test = train_test_split(X, y, d, test_size=tsize)
     
-    return X, y, d
-
-
-def dgp_ate4_test(n, var_e):
-    np.random.seed()
-    #treatment (randomly assigned)
-    d = np.random.choice([0, 1], size=(n,1), p=[1./2, 1./2])
-    #covariates (relevate to T.E.)
-    x1 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    x2 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    #covariates (relevant to outcome)
-    x3 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    x4 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    x5 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    #covariates (irrelevant)
-    x6 = np.random.normal(loc = 0.0, scale = 1.0, size = (n,1))
-    #error term
-    e = np.random.normal(loc = 0.0, scale = var_e, size = (n,1))
-
-    #indicator functions
-    ind1 = np.zeros((n,1))
-    ind2 = np.zeros((n,1))
-    ind3 = np.zeros((n,1))
-    ind1[np.where(x1 >= 0)] = 1
-    ind2[np.where(x2 >= 0)] = 1
-    ind3[np.where((x1 >= 0) & (x2 >= 0))] = 1
-
-    y = np.add(np.add(np.add(np.add(np.add(d*-3, np.multiply(np.add(np.add(4*ind1, ind2), ind3),d)),x3),x4),x5),e)
-    X = np.concatenate((x1,x2,x3,x4,x5,x6), axis=1)
-    
-    return X, y, d
+    return x_train, x_test, y_train, y_test, treat_train, treat_test
